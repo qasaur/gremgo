b77ac33303e22bfb2a3e6022dd19b5dddaa28ba3
Marcus Engvall <marcus@engvall.io>
2016-05-24 15:10:03 +0200

    Added foundation for client testing

--- client_test.go
+++ client_test.go
@@ -1,1 +1,25 @@
 package gremgo
+
+import "testing"
+
+type dummyConnector struct {
+	msg []byte
+}
+
+func (c *dummyConnector) connect() (err error) {
+	return
+}
+
+func (c *dummyConnector) write(msg []byte) (err error) {
+	c.msg = msg
+	return
+}
+
+func (c *dummyConnector) read() (msg []byte, err error) {
+	msg = c.msg
+	return
+}
+
+func TestStandardRequest(t *testing.T) {
+	return
+}
